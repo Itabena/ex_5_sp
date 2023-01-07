dddc9e0ab134b9fcdd4c3ef6f0ce668737e139fe
Itabena <itamar.benarosh@mail.huji.ac.il>
2023-01-07 09:09:09 +0200

    changes
    Plotting results - calling data from simulation and using functions to plot the results.
    Simulation- the main script with the simulation functions and data saving

---
+++ Plotting_and_results.py
@@ -0,0 +1,159 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import json
+from Projects.Bcs_physics.Numeric_statistical_mechanics.ex_5.Simulation import Calc_specific_heat
+
+#region - Paths
+result_path='C:\\Users\\ita-no\\My Drive\\BCs Physics\\3rd year\\Introduction to statistical physics\\Numerical ex\\ex4\\results\\'
+data_path='C:\\Users\\ita-no\\My Drive\\BCs Physics\\3rd year\\Introduction to statistical physics\\Numerical ex\\ex4\\data\\'
+#endregion
+# region -Importiong simulated data
+h=[0,0.1,0.5,1.0]
+#region - h=0.1
+M_h_01=[]
+M_s_h_01=[]
+U_h_01=[]
+U_s_h_01=[]
+cv_h_01=[]
+eta_h_01=[]
+for i in [0.30,0.55,0.75]:
+  with open(data_path+'h= '+str(h[1])+'_field_eta_vs_data_until_'+ "{:.2f}".format(i)+'.txt','rt') as file:
+     tmp_dict=json.load(file)
+M_h_01.extend(tmp_dict["M"])
+M_s_h_01.extend(tmp_dict["Ms"])
+U_h_01.extend(tmp_dict["U"])
+U_s_h_01.extend(tmp_dict["Us"])
+cv_h_01.extend(tmp_dict["Cv"])
+eta_h_01.extend(tmp_dict["eta"])
+#endregion
+tmp_dict={}
+#region - h=0.5
+M_h_05=[]
+M_s_h_05=[]
+U_h_05=[]
+U_s_h_05=[]
+cv_h_05=[]
+eta_h_05=[]
+for i in [0.30,0.55,0.75]:
+  with open(data_path+'h= '+str(h[2])+'_field_eta_vs_data_until_'+ "{:.2f}".format(i)+'.txt','rt') as file:
+     tmp_dict=json.load(file)
+M_h_05.extend(tmp_dict["M"])
+M_s_h_05.extend(tmp_dict["Ms"])
+U_h_05.extend(tmp_dict["U"])
+U_s_h_05.extend(tmp_dict["Us"])
+cv_h_05.extend(tmp_dict["Cv"])
+eta_h_05.extend(tmp_dict["eta"])
+#endregion
+tmp_dict={}
+#region - h=1.0
+M_h_10=[]
+M_s_h_10=[]
+U_h_10=[]
+U_s_h_10=[]
+cv_h_10=[]
+eta_h_10=[]
+for i in [0.30,0.55,0.75]:
+  with open(data_path+'h= '+str(h[3])+'_field_eta_vs_data_until_'+ "{:.2f}".format(i)+'.txt','rt') as file:
+     tmp_dict=json.load(file)
+M_h_10.extend(tmp_dict["M"])
+M_s_h_10.extend(tmp_dict["Ms"])
+U_h_10.extend(tmp_dict["U"])
+U_s_h_10.extend(tmp_dict["Us"])
+cv_h_10.extend(tmp_dict["Cv"])
+eta_h_10.extend(tmp_dict["eta"])
+#endregion
+tmp_dict={}
+#region - h=0
+M_h_0=[]
+M_s_h_0=[]
+U_h_0=[]
+U_s_h_0=[]
+cv_h_0=[]
+eta_h_0=[]
+for i in [0.30,0.55,0.80]:
+  with open(data_path+'h= '+str(h[0])+'_field_eta_vs_data_until_'+ "{:.2f}".format(i)+'.txt','rt') as file:
+     tmp_dict=json.load(file)
+M_h_0.extend(tmp_dict["M"])
+M_s_h_0.extend(tmp_dict["Ms"])
+U_h_0.extend(tmp_dict["U"])
+U_s_h_0.extend(tmp_dict["Us"])
+cv_h_0.extend(tmp_dict["Cv"])
+eta_h_0.extend(tmp_dict["eta"])
+#endregion
+tmp_dict={}
+#region - h=0,Around critiacl
+M_h_0_HR=[]
+M_s_h_0_HR=[]
+U_h_0_HR=[]
+U_s_h_0_HR=[]
+cv_h_0_HR=[]
+eta_h_0_HR=[]
+for i in [0.44,0.47]:
+  with open(data_path+'h= '+str(h[0])+'_field_eta_vs_data_until_HR_'+ "{:.2f}".format(i)+'.txt','rt') as file:
+     tmp_dict=json.load(file)
+M_h_0_HR.extend(tmp_dict["M"])
+M_s_h_0_HR.extend(tmp_dict["Ms"])
+U_h_0_HR.extend(tmp_dict["U"])
+U_s_h_0_HR.extend(tmp_dict["Us"])
+cv_h_0_HR.extend(tmp_dict["Cv"])
+eta_h_0_HR.extend(tmp_dict["eta"])
+#endregion
+#endregion
+#region - Other important variables
+n=32
+N=n**2
+#endregion
+#region -Plotting functions
+def Plot_Data(x,y,save_dir=' ',save_fig=0,new_fig=0,log=0):
+    '''
+    :param x: The data of X axis.
+    :param y: The data of Y axis.
+    :param save_dir: ' ' by defult, The path for the saving folder.
+    :param save_fig: 0 by defult ,Type 1 if save intended.
+    :param new_fig:  0 by defult ,Type 1 if you wish to create a nex figure.
+    :param log: 0 by defult , Type 1 if yo wish to create semi-log ,Type -1 to create log
+    :return: Plots the wanted figures
+    '''
+
+    if new_fig==0:
+       plt.figure()
+    print('The lagand of the ploted data: ')
+    plot_name=input()
+    plt.plt(x,y,'*',label=plot_name)
+    if log==1:
+        plt.yscale('log')
+        plt.xscale('log')
+        plt.grid()
+    elif log==-1:
+        plt.yscale('log')
+        plt.grid()
+    else:
+        plt.grid()
+    print('The x Axis label is: ')
+    x_name=input()
+    print('The y Axis label is: ')
+    y_name=input()
+    plt.xlabel(x_name,fontsize=15)
+    plt.ylabel(y_name, fontsize=15)
+    if save_fig==0:
+       print('The plot file name :')
+       name = input()
+       plt.savefig(save_dir+name+'.png')
+    return
+def Onsager(eta_min,eta_max,num_res):
+   '''
+    :param eta_min: Minimum of eta list
+    :param eta_max: Maximum of eta list
+    :param num_res: number of elements in eta list
+    :return m:|M/N| claculated by Onsager analytical solution
+   '''
+   eta=np.linspace(eta_min,eta_max,num_res,endpoint=True)
+   z=np.exp(-2*eta)
+   m=((1+z**2)**(1/4))*((1-6*(z**2)+z**4)**(1/8))/((1-z**2)**(1/2))
+   return m
+
+#endregion
+ #region - Creating the final results
+
+#endregion
--- Simulation.py
+++ Simulation.py
@@ -1,8 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import json
 
+
+#paths
+result_path='C:\\Users\\ita-no\\My Drive\\BCs Physics\\3rd year\\Introduction to statistical physics\\Numerical ex\\ex4\\results\\'
+data_path='C:\\Users\\ita-no\\My Drive\\BCs Physics\\3rd year\\Introduction to statistical physics\\Numerical ex\\ex4\\data\\'
 #region - Assiting Functions
+def Timer(t_start,t_end):
+  if 1<((t_end-t_start)/60)<=60:
+   print('total time ran:',"{:.2f}".format((t_end-t_start)/60),'min')
+  elif ((t_end-t_start)/60)>60:
+   print('total time ran:',"{:.2f}".format((t_end-t_start)/60/60),'hours')
+  else:
+    print('total time ran:',"{:.2f}".format(t_end-t_start),'sec')
+  return
 def Calc_energy(lattice,i,j,eta,h):
        latt=lattice.lattice
        p=latt[i][j].s
@@ -104,7 +117,7 @@ class lattice:
 
 #endregion
 
-#region - Main
+#region - Simulate
 def Simulate_spin_resrviour(n,eta,h,nsweep,Kinit,delta):
 
 
@@ -164,9 +177,9 @@ def Simulate_spin_resrviour(n,eta,h,nsweep,Kinit,delta):
 
         if   K <= flips and clr==1:
 
-#clock
-            ttt = toc - tic
-            print('run time:', "{:.2f}".format(ttt), 's, so far.')
+##clock
+
+            # Timer(tic,toc)
 
 ##checks for the code:
             # print(delta_M,'||',delta)
@@ -196,42 +209,93 @@ def Simulate_spin_resrviour(n,eta,h,nsweep,Kinit,delta):
     return mean_M,mean_M_s,mean_U,mean_U_s,cv
 #endregion
 
+
+#region- Main
 #put this before the main
 t_start=time.time()
 #put the main here
-M,M_s,U,U_s,cv=Simulate_spin_resrviour(32,0.5,1,5,10000,0.001)
-#put this after the main
+eta2=np.arange(0.1,0.85,0.05)
+eta1=np.arange(0.42,0.465,0.005)
+eta3=np.arange(0.46,0.8,0.05)
+
+M_list=[]
+M_s_list=[]
+U_list=[]
+U_s_list=[]
+cv_list=[]
+# Open a file to write the results to
+save_dict=dict()
+
+# with open('results.txt', 'w') as f:
+# Iterate over the values of eta1
+print('Starting')
+for h in [0]:
+ cnt=0
+ for i in range(0,len(eta1)):
+        cnt+=1
+        # Run the simulation and save the results
+        tik=time.time()
+        M,M_s,U,U_s,cv=Simulate_spin_resrviour(32, eta1[i], h, 5, 10000, 10**(-3))
+        tok=time.time()
+        Timer(tik,tok)
+        print('Eta value:',"{:.2f}".format(eta1[i]),'M is: ',"{:.1f}".format(M),'simulation time was',)
+        M_list.append(M)
+        M_s_list.append(M_s)
+        U_list.append(U)
+        U_s_list.append(U_s)
+        cv_list.append(cv)
+        if cnt%5 == 0 or i==len(eta1)-1:
+            save_dict['M']=M_list
+            save_dict['Ms'] = M_s_list
+            save_dict['U'] = U_list
+            save_dict['Us'] = U_s_list
+            save_dict['Cv'] = cv_list
+            save_dict['eta'] = eta1[cnt-5:cnt].tolist()
+            with open(data_path +'h= '+str(h)+'_field_eta_vs_data_until_HR_'+ "{:.2f}".format(eta1[i])+'.txt','wt') as file:
+               json.dump(save_dict,file,indent=4)
+            save_dict.clear()
+            M_list=[]
+            M_s_list=[]
+            U_list=[]
+            U_s_list=[]
+            cv_list=[]
+        # Write the results to the file
+        # f.write(f"{M}, {M_s}, {U}, {U_s}, {cv}\n")
+
+#
+#     # Iterate over the values of eta2
+#     for i in range(0,len(eta2)-1):
+#         # Run the simulation and save the results
+#         M,M_s,U,U_s,cv=Simulate_spin_resrviour(32, eta2[i], 0, 5, 10000, 10^(-3))
+#         M_list.append(M)
+#         M_s_list.append(M_s)
+#         U_list.append(U)
+#         U_s_list.append(U_s)
+#         cv_list.append(cv)
+#         # Write the results to the file
+#         f.write(f"{M}, {M_s}, {U}, {U_s}, {cv}\n")
+#
+#     # Iterate over the values of eta3
+#     for i in range(0,len(eta3)-1):
+#         # Run the simulation and save the results
+#         M,M_s,U,U_s,cv=Simulate_spin_resrviour(32, eta3[i], 0, 5, 10000, 10^(-3))
+#         M_list.append(M)
+#         M_s_list.append(M_s)
+#         U_list.append(U)
+#         U_s_list.append(U_s)
+#         cv_list.append(cv)
+#         # Write the results to the file
+#         f.write(f"{M}, {M_s}, {U}, {U_s}, {cv}\n")
+#
+# print("M: " , M_list)
+# print("M_s: " , M_s_list)
+# print("U: " , U_list)
+# print("U_s: " , U_s_list)
+# print("cv: " , cv_list)
+# #put this after the main
 t_end=time.time()
-if 1<((t_end-t_start)/60)<=60:
- print('total time ran:',"{:.2f}".format((t_end-t_start)/60),'min')
-elif ((t_end-t_start)/60)>60:
- print('total time ran:',"{:.2f}".format((t_end-t_start)/60/60),'hours')
-else:
-    print('total time ran:',"{:.2f}".format(t_end-t_start),'sec')
+Timer(t_start,t_end)
 # print(M,M_s,U,U_s,cv)
+#endregion
 
 
-
-#example of how to use the class
-# latt=lattice(n)
-# latt.Scatter()
-# latt2=lattice(n)
-# latt2.Scatter()
-# w=latt.lattice[1][1].s
-# y=latt2.lattice[1][1].s
-# print(w,y)
-# a=latt.lattice[1][1].flip()
-# b=latt2.flip(1,1)
-# print(a,b[1][1].s)
-
-
-#example for saving the data
-# with open(data_path+"name"+'.txt', 'wt') as file:
-#          json.dump(data_dict_for_save),file,indent=4)
-#data_dict_for_save={}
-#l=[m]
-#data_dict_for_save['M']=M
-#data_dict_for_save['M_s']=M_s
-# with open(data_path + "name" + '.txt', 'rt') as file:
-#     data_1000_a = json.load(file)
-#M= data_1000_a['M']
